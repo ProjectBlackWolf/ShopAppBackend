9a17b921e1c5dff13584d6c71a5d74c95d9c80a2
patteesolomon <patteesolomon@gmail.com>
2023-10-31 14:25:56 -0500

    thing

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import db from './db/index.js';
 import dotenv from 'dotenv';
 import { getDirName } from "./getDirName.js";
 import morgan from 'morgan';
-import validInfo from "./validInfo.js";
-import JwtSeed from './JwtSeed.js';
-// const port = process.env.PORT || 3005;
-
-const dirName = getDirName(import.meta.url);
+import validInfo  from "./validInfo.js";
+import bcrypt from 'bcrypt';
+// import jwtAuth from './JwtAuth.js'; these are unused - don't import them here.
+// import jwtSeed from './JwtSeed.js'; 
+const port = process.env.PORT || 3005;
 const app = express();
+const dirName = getDirName(import.meta.url);
 /*
 Express Middleware
 
@@ -20,9 +21,7 @@ Express Middleware
                         
 */
 dotenv.config();
-const port = 3000; // port always comes after dotenv
-
-//induces hierarchy 
+//induces order 
 //use
 //set 
 //engine
@@ -36,8 +35,7 @@ const port = 3000; // port always comes after dotenv
 // creates a new middle ware function to override
 // the req method property with a new value
 app.use(methodOverride("_method"));
-// app.use("/authentication", require("./jwtAuth"));
-// app.use("/dashboard", require("./dashboard"));
+
 //app.use('/public', express.static('public'));
 app.use(express.static(dirName + '/public')); // Keep
 app.use(cors());
@@ -53,7 +51,7 @@ app.use((req, res, next) => {
 
 app.delete("/invItem/:id", async (req, res) => {
     try {
-        const results = db.query("DELETE FROM product where id = $1;", [
+        const results = db.query("DELETE FROM product where id = $1", [
             req.params.id
         ]);
         res.status(204).json({
@@ -65,10 +63,10 @@ app.delete("/invItem/:id", async (req, res) => {
     }
 })
 
-// // deleting an item
-app.delete('/orders/:id', async (req, res) => {
+// deleting an item
+app.delete('/cart/:id', async (req, res) => {
     try {
-        const results = db.query("DELETE FROM orders where id = $1;", [
+        const results = db.query("DELETE FROM users where id = $1", [
             req.params.id
         ])
     res.status(204).json({
@@ -80,64 +78,12 @@ app.delete('/orders/:id', async (req, res) => {
     }
 })
 
-// update
-// yes just because of this assignment
-app.put("/invItem/:id", async (req, res) => {
-    try {
-        const results = await db.query(
-            "UPDATE product SET name = $1, price = $2, description = $3, image = $4, quantity = $5, category_id = $6, sku = $7 where id = $8 returning *;",
-            [req.body.name, req.body.price, req.body.description,
-            req.params.image, req.params.quantity,
-            req.params.category_id, req.params.sku, req.params.id]
-        );
-        res.status(200).json({
-            status: "success",
-            data: {
-                item: results.rows[0]
-            }
-        });
-    } catch (err) {
-        console.log(err);
-    }
-    console.log(req.params.id);
-    console.log(req.body);
-});
-
-// updating the cart
-// updating info in the cart
+// 'add to cart'
 app.put("/cart/:id", async (req, res) => {
     try {
-        const results = await db.query(
-            "UPDATE orders SET userid = $1, orderid = $2, quantity = $3 where id = $4 returning *;",
-            [
-                req.body.userid, req.body.orderid,
-                req.body.quantity, req.body.id
-            ]
-        );
-        res.send(
-            {
-                status: "success",
-                data: {
-                    order: results.rows[0]
-                }
-            }
-        )
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-// 'add to cart'
-
-app.post("/cart/:id", async (req, res) => {
-    try {
-        const results = await db.query(`INSERT INTO orders (userid, itemid, quantity, id) values ($1, $2, $3, $4) returning *;`, [
-            req.params.userid,
-            req.params.itemid,
-            req.params.quantity,
+        const results = await db.query("UPDATE cart SET name = $1, price = $2, description = $3, image = $4, quantity = $5 where id = $6 returning *", [
             req.params.id
         ]);
-
         res.status(200).json({
             status: "success",
             data: {
@@ -149,16 +95,36 @@ app.post("/cart/:id", async (req, res) => {
     }
 })
 
-// create a user
+// update
+// yes just because of this assignment
+app.put("/invItem/:id", async (req, res) => {
+    try {
+        const results = await db.query(
+            "UPDATE product SET name = $1, price = $2, description = $3, image = $4, quantity = $5, category_id = $6, sku = $7 where id = $8 returning *",
+            [req.body.name, req.body.price, req.body.description, req.params.image, req.params.quantity, req.params.category_id, req.params.sku, req.params.id]
+        );
+        res.status(200).json({
+            status: "success",
+            data: {
+                item: results.rows[0]
+            }
+        });
+        res.json("Updated Item.");
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(req.params.id);
+    console.log(req.body);
+});
+
+// create a users
 app.post("/users", async (req, res) => {
     // getting userdata from table
     // 
     try {
-        const send = await db.query(`INSERT INTO users(id, us, ps) values ($1, $2, $3) returning *;`,
+        const send = await db.query(`INSERT INTO users(id, us, ps, cartInv) values ($1, $2, $3, $4) returning *`,
             [
-                req.body.id,
-                req.body.us,
-                req.body.ps
+                req.body.id, req.body.us, req.body.ps, req.body.cartInv
             ]
         );
         console.log(send);
@@ -174,11 +140,11 @@ app.post("/users", async (req, res) => {
 })
 
 app.post("/signup", validInfo, async (req, res) => {
-    const { us, ps } = req.body;
+    const { us, ps, cartInv } = req.body;
+
     try {
-        const user = await db.query("SELECT * FROM users WHERE us = $1;", [
-            us,
-            ps
+        const user = await db.query("SELECT * FROM users WHERE us = $1", [
+            us
         ]);
         
         if (user.rows.length > 0) {
@@ -189,8 +155,8 @@ app.post("/signup", validInfo, async (req, res) => {
         const bcryptPassword = await bcrypt.hash(ps, salt);
 
         let newUser = await db.query(
-            "INSERT INTER users (us, ps, cart) VALUES ($1, $2, $3) RETURNING *;",
-            [us, bcryptPassword]
+            "INSERT INTER users (us, ps, cart) VALUES ($1, $2, $3) RETURNING *",
+            [us, bcryptPassword, cartInv]
         );
 
         const jwtToken = jwtGenerator(newUser.rows[0].us);
@@ -204,110 +170,44 @@ app.post("/signup", validInfo, async (req, res) => {
 // create 
 app.post("/invItem", async (req, res) => {
     try {
-        
-        const results = await db.query(`INSERT INTO product(id, name, price, quantity, 
-            description, image, category_id, sku) values ($1, $2, $3, $4, $5, $6, $7, $8) returning *;`,
+        const results = await db.query(`INSERT INTO product(id, name, price, description, image, quantity, category_id, sku) values ($1, $2, $3, $4, $5, $6, $7, $8) returning *`,
             [
-                req.body.id, req.body.name, req.body.price, req.body.quantity,
-                req.body.description, req.body.image,
-                req.body.category_id, req.body.sku
+                req.body.id, req.body.name, req.body.price, req.body.description, req.body.image,
+                req.body.quantity, req.body.category_id, req.body.sku
             ]
         );
         console.log(results);
         res.status(201).json({
             status: "success",
             data: {
-                item: results.rows[0]
-            }
+                item: results.rows[0],
+            },
         });
     } catch (error) {
         console.log(error);
     }
-});
+})
 
 // welcome
 app.get('/', (req, res) => {
     res.send("hola");
 })
 
-// make a table called 'orders' and 'users'.
-// orders: with the values productId,
-// and make the routes get, post, update and delete
-
-app.get("/users", async (req, res) => {
+app.get('/users', async (req, res) => {
     try {
-        const users = await db.query('SELECT * FROM users;');
-        console.log(users);
-        res.json(users.rows);
+        const userbase = await db.query('SELECT * FROM users');
     } catch (error) {
-        console.log(error);
+        
     }
-});
-
-// getting the right user
-app.get('/users/:id', async (req, res) => {
-    try {
-        const userC = await db.query(`SELECT * FROM users WHERE $1 = ;`, [req.body.id]);
-        res.json(userC);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-// grabbing one users stuff.
-app.get('/orders/stuff/:userid', async (req, res) => {
-    try {
-        const orders = await db.query(`SELECT * FROM orders WHERE userid = ;`, [req.body.userid]);
-        console.log(orders);
-        res.status(200).json({
-            status: "success",
-            data: {
-                order: results.rows[0]
-            }
-        });
-        res.json(orders.rows);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-//order id
-app.get('/orders/:id', async (req, res) => {
-    try {
-        const orders =
-            await db.query(`SELECT * FROM orders WHERE id = $1;`,
-                [req.body.id]);
-        console.log(orders);
-        res.status(200).json({
-            status: "success",
-            data: {
-                order: results.rows[0]
-            }
-        });
-        res.json(orders.rows);
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-app.get("/invItem", async(req, res)=>
-{
-    res.send('Context');
 })
-
-app.get("/THANKS", async (req, res) => {
-    res.send("Arigato.")
-});
 
 // GET ALL\ items
 app.get("/invItem/getAll", async (req, res) => {
     try {
-        const itemas = await db.query('SELECT * FROM product;');
+        const itemas = await db.query('SELECT * FROM product');
         console.log(itemas);
         res.json(itemas.rows);
-    } catch (error) {
-        console.log(error);
-    }
+    } catch (error) { console.log(error); }
 });
 
 // Get one specific item
@@ -316,7 +216,7 @@ app.get("/invItem/show/:id", async (req, res) => {
     console.log(req.body);
     try {
         const results =
-            await db.query(`SELECT * FROM product WHERE id = $1;`,
+            await db.query(`SELECT * FROM product WHERE id = $1`,
                 [req.params.id]);
         console.log(results);
         res.status(200).json({
--- validInfo.js
+++ validInfo.js
@@ -1,18 +1,23 @@
-export default function(req, res, next) {
-    const { nameS, password } = req.body;
 
-    function validUs(nameS) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(nameS);
+export default function(req, res, next) {
+    const { email, name, password } = req.body;
+
+    function validEmail(userEmail) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
     }
 
     if (req.path === "/register") {
-        console.log(!nameS.length);
-        if (![nameS, password].every(Boolean)) {
+        console.log(!email.length);
+        if (![email, name, password].every(Boolean)) {
         return res.json("Missing Credentials");
+        } else if (!validEmail(email)) {
+        return res.json("Invalid Email");
         }
     } else if (req.path === "/login") {
-        if (![nameS, password].every(Boolean)) {
+        if (![email, password].every(Boolean)) {
         return res.json("Missing Credentials");
+        } else if (!validEmail(email)) {
+        return res.json("Invalid Email");
         }
     }
 
